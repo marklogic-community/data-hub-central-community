1372b92cffe24bdc3f95033769d15e44ee1a4257
Paxton Hare <paxton@greenllama.com>
2020-05-19 11:10:09 -0400

    adding travis to run tests

---
+++ .travis/run-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
+	project_dir=$(pwd)
+	tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+	echo "Temp Dir: ${tmp_dir}"
+	cd $tmp_dir
+
+	mkdir conceptConnectorModels
+
+	echo "plugins {
+		id 'net.saliman.properties' version '1.4.6'
+		id 'com.marklogic.ml-data-hub' version '5.2.0'
+	}" > build.gradle
+
+	gradle hubInit
+
+	echo "mlUsername=admin
+mlPassword=admin
+" > gradle-local.properties
+	gradle mlDeploy -i
+	gradle mlLoadModules -i
+
+	cd $project_dir
+  ./gradlew -DdhfDir=${tmp_dir} -DmodelsDir=${tmp_dir}/conceptConnectorModels clean test
+fi
