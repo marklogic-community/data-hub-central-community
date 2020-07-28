062d9140eeaffa681d4d607646a6fdb2531ba34a
Paxton Hare <paxton@greenllama.com>
2020-07-28 16:41:29 -0400

    fixing tests

--- ui/tests/e2e/specs/mastering.js
+++ ui/tests/e2e/specs/mastering.js
@@ -370,6 +370,7 @@ describe('Mastering', () => {
 		cy.route('POST', '/api/mastering/blocks', {"/doc1.json":["/doc2.json"],"/doc2.json":["/doc1.json"]})
 		cy.route('PUT', '/api/mastering/notifications', {})
 		cy.visit('/notifications/compare?notification=%2Fnote1.xml')
+		cy.wait(1000)
 
 		cy.get('th.doc0').should('contain', '/doc1.json')
 		cy.get('th.doc1').should('contain', '/doc2.json')
