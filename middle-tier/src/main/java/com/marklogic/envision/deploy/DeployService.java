36c2e7f1e24cdce563ce836bd7644da6f91dba0e
Paxton Hare <paxtonhare@users.noreply.github.com>
2020-08-12 11:33:29 -0400

    automatically install envision modules when being updated to a new version (#91)

--- middle-tier/src/main/java/com/marklogic/envision/deploy/DeployService.java
+++ middle-tier/src/main/java/com/marklogic/envision/deploy/DeployService.java
@@ -1,21 +1,33 @@
 package com.marklogic.envision.deploy;
 
+import com.marklogic.appdeployer.AppConfig;
 import com.marklogic.appdeployer.AppDeployer;
 import com.marklogic.appdeployer.impl.SimpleAppDeployer;
 import com.marklogic.envision.commands.DeployEntitiesCommand;
 import com.marklogic.envision.commands.DeployEnvisionModulesCommand;
+import com.marklogic.envision.config.EnvisionConfig;
 import com.marklogic.hub.impl.HubConfigImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class DeployService {
 
+	private final HubConfigImpl hubConfig;
+	private final EnvisionConfig config;
+
 	@Autowired
-	private HubConfigImpl hubConfig;
+	DeployService(HubConfigImpl hubConfig, EnvisionConfig config) {
+		this.hubConfig = hubConfig;
+		this.config = config;
+	}
 
 	public boolean needsInstall() {
-		return hubConfig.getAppConfig().newModulesDatabaseClient().newDocumentManager().exists("/envision/search/findEntities.sjs") == null;
+		String installedVersion = config.getInstalledVersion(hubConfig.newModulesDbClient());
+		return installedVersion == null || !installedVersion.equals(config.getVersion());
 	}
 
 	public void deployEntities() {
@@ -31,6 +43,8 @@ public class DeployService {
 	public void deploy() {
 		try {
 			AppDeployer appDeployer = new SimpleAppDeployer(hubConfig.getManageClient(), hubConfig.getAdminManager(), new DeployEnvisionModulesCommand(hubConfig));
+			AppConfig appConfig = hubConfig.getAppConfig();
+			appConfig.getCustomTokens().put("%%envisionVersion%%", config.getVersion());
 			appDeployer.deploy(hubConfig.getAppConfig());
 		}
 		catch (Error error) {
