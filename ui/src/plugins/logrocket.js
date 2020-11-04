0a6a11456401630d34a001bd73ded033919a8cb0
Paxton Hare <paxtonhare@users.noreply.github.com>
2020-11-04 13:08:38 -0500

    Logrocket (#116)
    * adding logrocket and google analytics
    
    * fixing issue when logrocket not defined

---
+++ ui/src/plugins/logrocket.js
@@ -0,0 +1,19 @@
+import store from '../store'
+
+const isHosted = process.env.VUE_APP_IS_HOSTED === 'true'
+const logRocketID = process.env.VUE_APP_LOGROCKET_ID
+const LogRocket = (isHosted && logRocketID) ? require('logrocket') : {
+	identify: function() {},
+	init: function() {}
+}
+
+export default {
+	install(Vue) {
+		Vue.prototype.$logRocket = this
+		store.$logRocket = this
+		LogRocket.init(logRocketID)
+	},
+	identify(id, userInfo) {
+		LogRocket.identify(id, userInfo)
+	}
+}
