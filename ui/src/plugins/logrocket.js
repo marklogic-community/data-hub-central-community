72447d86583bb0683d50e458fbd690ae7a716c56
Paxton Hare <paxtonhare@users.noreply.github.com>
2020-12-14 09:14:00 -0500

    Issue 27 (#138)
    * fixes issue #27
    adding version number to the UI
    
    * adding version to banner
    
    * adding version to logrocket
    obfuscating user input
    not logging http response data

--- ui/src/plugins/logrocket.js
+++ ui/src/plugins/logrocket.js
@@ -1,8 +1,8 @@
 import store from '../store'
 
-const isTesting = process.env.NODE_ENV === 'test'
+const isHosted = process.env.VUE_APP_IS_HOSTED === 'true'
 const logRocketID = process.env.VUE_APP_LOGROCKET_ID
-const LogRocket = (logRocketID && !isTesting) ? require('logrocket') : {
+const LogRocket = logRocketID ? require('logrocket') : {
 	identify: function() {},
 	init: function() {}
 }
@@ -11,7 +11,25 @@ export default {
 	install(Vue) {
 		Vue.prototype.$logRocket = this
 		store.$logRocket = this
-		LogRocket.init(logRocketID)
+		LogRocket.init(logRocketID, {
+			network: {
+				requestSanitizer: request => {
+					request.headers['Authorization'] = null;
+					if (!isHosted) {
+						request.body = null
+					}
+					else if (request.url.toLowerCase().indexOf('login') !== -1) {
+						request.body = null;
+					}
+
+					// otherwise log the request normally
+					return request;
+				},
+				responseSanitizer: () => {
+					return null;
+				},
+			},
+		})
 	},
 	identify(id, userInfo) {
 		LogRocket.identify(id, userInfo)
