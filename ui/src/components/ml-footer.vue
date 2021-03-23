562071200301054666e6493c079c8e6b3058f867
Sanjeevani19 <sanjeevani.vishaka@marklogic.com>
2021-03-23 14:30:18 -0500

    DHFPROD-6993:dhcce login page (#212)

--- ui/src/components/ml-footer.vue
+++ ui/src/components/ml-footer.vue
@@ -1,10 +1,16 @@
 <template>
 	<v-footer
-    dark color="secondary">
+     color="transparent">
     <v-container mx-auto>
-      <v-layout row>
-				<div>Copyright <sup>&copy;</sup> {{ currentYear }} MarkLogic Corporation.</div>
-      </v-layout>
+    <v-layout row class="footer">
+				<div>
+                  <span>© {{currentYear}} MarkLogic Corporation</span>
+                  |
+                  <span>
+                    <a href="https://www.marklogic.com/privacy/" class="link">Privacy</a>
+                  </span>
+          </div>
+				</v-layout>
     </v-container>
   </v-footer>
 </template>
@@ -19,3 +25,23 @@ export default {
   }
 };
 </script>
+
+<style>
+.footer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    color: rgb(153, 153, 153);
+ }
+
+ .link{
+  color: rgb(68, 73, 156);
+  text-decoration: none;
+ }
+
+ .link:hover{
+  color: #7fade3;
+ }
+</style>
+
